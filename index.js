fa3e171465eb52dcf2b669e7664f829a1a87fabf
Manes <manesgames@hotmail.com>
2018-08-01 17:14:44 +0200

    FirstCommit

---
+++ index.js
@@ -0,0 +1,26 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online`);
+  bot.user.setActivity("/Arma para Build");
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hola`){
+      return message.channel.send("Coño, un eidolon que habla !");
+    }
+
+});
+
+bot.login(process.env.token);
